050db900843c8e69407ac1ef188e0bdb23e6da30
qiujinyuan <yuangu.1994@gmail.com>
2019-09-16 00:52:42 +0800

    learn: 1. webpack code-splitting and caching

--- src/index.js
+++ src/index.js
@@ -1,4 +1,100 @@
-import { join } from 'lodash';
+// import printMe from './print.js';
+// import './styles.css';
 
-// eslint-disable-next-line no-console
-console.log(join('a', 'b', 'c'));
+/**
+ * caching
+ */
+import _ from 'lodash';
+// import Print from './print';
+
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.onclick = Print.bind(null, 'Hello webpack!');
+
+  return element;
+}
+
+document.body.appendChild(component());
+
+/**
+ * lazy loading
+ */
+// import _ from 'lodash';
+
+// function component() {
+//   const element = document.createElement('div');
+//   const button = document.createElement('button');
+//   const br = document.createElement('br');
+//   button.innerHTML = 'Click me and look ad the console!';
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//   element.appendChild(br);
+//   element.appendChild(button);
+
+//   button.onclick = () =>
+//     // eslint-disable-next-line implicit-arrow-linebreak
+//     import(/* webpackChunkName: "print" */ './print').then((module) => {
+//       const print = module.default;
+//       print();
+//     });
+
+//   return element;
+// }
+
+// document.body.appendChild(component());
+
+/**
+ * code splitting
+ */
+
+// async function getComponent() {
+//   const { default: _ } = await import(
+//     /* webpackChunkName: "lodash" */ 'lodash'
+//   );
+//   const element = document.createElement('div');
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//   return element;
+// }
+
+// getComponent()
+//   .then((component) => {
+//     document.body.appendChild(component);
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
+
+/**
+ * HMR
+ */
+
+// function component() {
+//   const element = document.createElement('div');
+//   const btn = document.createElement('button');
+
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+//   btn.innerHTML = 'Click me and check the console!';
+//   btn.onclick = printMe;
+
+//   element.appendChild(btn);
+
+//   return element;
+// }
+
+// document.body.appendChild(component());
+// let element = component();
+// document.body.appendChild(element);
+
+// if (module.hot) {
+//   module.hot.accept('./print.js', () => {
+//     // eslint-disable-next-line no-console
+//     console.log('Accepting the updated printMe module!');
+//     // printMe();
+//     document.body.removeChild(element);
+//     element = component();
+//     document.body.appendChild(element);
+//   });
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,59 @@
-const webpack = require('webpack');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const path = require('path');
 
 module.exports = {
   target: 'web',
-  entry: './src/index.js',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].[hash].js',
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
   },
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.txt$/,
-  //       use: 'raw-loader',
-  //     },
-  //     {
-  //       test: /\.css$/,
-  //       use: [
-  //         {
-  //           loader: 'style-loader',
-  //         },
-  //         {
-  //           loader: 'css-loader',
-  //         },
-  //         {
-  //           loader: 'sass-loader',
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       test: /\.(js|jsx)$/,
-  //       use: 'babel-loader',
-  //     },
-  //   ],
-  // },
-  // plugins: [
-  //   new config.optimization.splitChunks({
-  //     name: 'vendor',
-  //   }),
-  // ],
+  // devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+  output: {
+    filename: '[name].[contentHash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      // {
+      //   test: /\.txt$/,
+      //   use: 'raw-loader',
+      // },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      // {
+      //   test: /\.(js|jsx)$/,
+      //   use: 'babel-loader',
+      // },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      // title: 'Development',
+      title: 'Caching',
+    }),
+    // new BundleAnalyzerPlugin(),
+  ],
+  optimization: {
+    moduleIds: 'hashed',
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
 };
